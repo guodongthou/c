dc21904a6b42283ad176daaeb58e0f110c9f3d7d
guodongthou <2040262395@qq.com>
2022-07-23 18:56:18 +0800

    change

--- c.home/daikuan.c
+++ c.home/daikuan.c
@@ -5,6 +5,6 @@ int main()
     int yue,zongjin;
     scanf("%d %lf %d",&zongjin,&lilv,&yue);
     double s=zongjin*10000/yue+(zongjin*10000)*lilv;
-    printf("%.0lf",s);
+    printf("%.lf",s);
     return 0;
 }
