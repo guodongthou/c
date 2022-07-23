8230e7f06e2df4d4895e2f88a616863fbb21d03d
guodongthou <2040262395@qq.com>
2022-07-23 21:57:27 +0800

    change

--- c.home/jingdu.c
+++ c.home/jingdu.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    char a;
-    int b;
-    float c;
-    double d;
+    char a;    //定义一个字符
+    int b;    //定义一个整型变量
+    float c;    //定义一个单精度浮点小数
+    double d;    //定义一个双精度浮点小数
     scanf("%c %d %f %lf",&a,&b,&c,&d);
-    printf("%c %4d %.2f %.12lf",a,b,c,d);
+    printf("%c %4d %.2f %.12lf",a,b,c,d);    //占4个字符，向右对齐
     return 0;
 }
