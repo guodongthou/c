c67fe40429f097f8ae20f84667b556ca474942b7
guodongthou <2040262395@qq.com>
2022-07-24 10:45:45 +0800

    add code

---
+++ c.home/max.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a<b)
+    d=b;
+    else
+    d=a;
+    if(d<c)
+    d=c;
+    printf("%d\n",d);
+    return 0;
+}
