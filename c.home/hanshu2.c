d8f3cdb854e237d39fc5e24b2b67e61d4566d2b1
guodongthou <2040262395@qq.com>
2022-07-26 19:54:44 +0800

    add code

---
+++ c.home/hanshu2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+    int x,y;
+    scanf("%d",&x);
+    if(x<1)
+        y=x;
+    else if(1<=x&&x<10)
+        y=2*x-1;
+    else
+        y=3*x-11;
+    printf("%d\n",y);
+}
