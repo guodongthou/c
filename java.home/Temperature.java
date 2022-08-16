e7c2141da8b8f6618e344d058a704b581841bbde
guodongthou <2040262395@qq.com>
2022-08-17 00:50:39 +0800

    add code

--- java.home/Temperature.java
+++ java.home/Temperature.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
-public class wendu {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+public class Temperature
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
         double F,C;
         F = sc.nextDouble();
         C = 5*(F-32)/9;
