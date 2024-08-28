b6bfbd615291ad4fe9d62dca1f534f74db2d40fd
guodongthou <2040262395@qq.com>
2024-08-28 21:03:08 +0800

    renamed:    "class/\347\254\2544\346\254\241\345\256\236\351\252\214-\345\276\252\347\216\257\347\273\223\346\236\204-\351\242\230\347\233\256/.vscode/settings.json" -> "C/class/\347\254\2544\346\254\241\345\256\236\351\252\214-\345\276\252\347\216\257\347\273\223\346\236\204-\351\242\230\347\233\256/.vscode/settings.json" 	renamed:    "class/\347\254\2544\346\254\241\345\256\236\351\252\214-\345\276\252\347\216\257\347\273\223\346\236\204-\351\242\230\347\233\256/\347\254\2544\346\254\241\345\256\236\351\252\214-\345\276\252\347\216\257\347\273\223\346\236\204-\351\242\230\347\233\256/un\345\220\257\345\217\221\346\200\247\345\256\236\351\252\214-blank.c" -> "C/class/\347\254\2544\346\254\241\345\256\236\351\252\214-\345\276\252\347\216\257\347\273\223\346\236\204-\351\242\230\347\233\256/\347\254\2544\346\254\241\345\256\236\351\252\214-\345\276\252\347\216\257\347\273\223\346\236\204-\351\242\230\347\233\256/un\345\220\257\345\217\221\346\200\247\345\256\236\351\252\214-blank.c"

--- oj/oj.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-main()
-{
-    double n, g, f;
-    scanf("%lf",&n);
-    g = 2 * n + 1;
-    f = (g + 1) * (g + 1);
-    printf("%.2f", f);
-}
--- python.home/wendu.py
+++
@@ -1,3 +0,0 @@
-n = eval(input())
-c = 5*(n-32)/9
-print("c = {:.2f}".format(c))
